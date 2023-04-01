6b4019ade30a3f6159ee693d311f5ce95c2380aa
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-01 02:28:51 -0300

    Create atvd6.java

---
+++ atvd6.java
@@ -0,0 +1,30 @@
+package Atividades;
+
+import java.util.Scanner;
+
+public class atv6 {
+
+	public static void main(String[] args) {
+		
+		
+		    Scanner sc = new Scanner(System.in);
+		    
+		    System.out.print("Quantos ano voce tem: ");
+		    int anos = sc.nextInt();
+		    
+		    System.out.print("Que mes voce nasceu: ");
+		    int meses = sc.nextInt();
+		    
+		    System.out.print("Dia que voce nasceu: ");
+		    int dias = sc.nextInt();
+		    
+		    int idadeEmDias = (anos * 365) + (meses * 30) + dias;
+		    
+		    System.out.println("A idade em dias é: " + idadeEmDias);
+		    
+		    sc.close();
+		  }
+		}
+
+		
+		
