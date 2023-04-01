1c5c20413384ec00b91520fe11079fed4ef5cb3f
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-01 02:29:06 -0300

    Create atvd7.java

---
+++ atvd7.java
@@ -0,0 +1,27 @@
+package Atividades;
+
+public class atv7 {
+
+	public static void main(String[] args) {
+		int a, b, c, x, y, z;
+		
+		x = 8;
+		y = 9;
+		z = 7;
+		
+		double media1 = (x+y+z)/3;
+		
+		a = 4;
+		b = 5;
+		c = 6;
+		
+		double media2 = (a+b+c)/3;
+		
+		double resultado = media1+media2;
+		
+		System.out.println(resultado);
+		
+
+	}
+
+}
