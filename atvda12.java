91752aa7edf9eee6fdb4f283f0c043fb81e31db8
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-01 02:31:47 -0300

    Create atvda12.java

---
+++ atvda12.java
@@ -0,0 +1,28 @@
+package Atividades;
+
+import java.util.Scanner;
+
+public class atvd12 {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 
+		 System.out.println("Digite um numero: ");
+		 double numero = sc.nextDouble();
+		 
+		 if(numero>=0) {
+			 System.out.println(numero+ " Positivo");
+		 } else
+			 System.out.println(numero+ " Negativo");
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+
+	}
+
+}
