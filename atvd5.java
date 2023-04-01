348e64cdf9966d5e47ce288203307a79ba85e9e0
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-01 02:28:36 -0300

    Create atvd5.java

---
+++ atvd5.java
@@ -0,0 +1,16 @@
+package Atividades;
+
+public class atv5 {
+	public static void main(String[] args) {
+		
+	int numero1 = 125;
+	int numero2 = 24;
+	
+	System.out.println(numero1+numero2);
+	System.out.println(numero1-numero2);
+	System.out.println(numero1*numero2);
+	System.out.println(numero1/numero2);
+	
+	
+	}
+}
