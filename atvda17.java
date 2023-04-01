bc21357c989b6c634facc0e16779a8117ad83f24
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-01 02:33:16 -0300

    Create atvda17.java

---
+++ atvda17.java
@@ -0,0 +1,19 @@
+package Atividades;
+
+import java.util.Scanner;
+
+public class atvd17 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite seu nome: ");
+		String nome = sc.next();
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(nome);
+		}
+		
+		
+		
+	}
+}
