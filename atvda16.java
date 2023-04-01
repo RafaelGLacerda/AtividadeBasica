1517c9c842cc863ee86f1c1e21dba1181faeab03
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-01 02:32:56 -0300

    Create atvda16.java

---
+++ atvda16.java
@@ -0,0 +1,21 @@
+package Atividades;
+
+public class atvd16 {
+	public static void main(String[] args) {
+   
+		int soma = 0;
+		
+		for(int i = 1; i < 15; i++) {
+			 soma += i;
+			
+		}
+		System.out.println(soma);
+		
+		
+		
+		
+		
+		
+		
+	}
+}
