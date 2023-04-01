2a0f8950a2225eff41e08d9f7c656920bdc1a418
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-01 02:32:42 -0300

    Create atvda15.java

---
+++ atvda15.java
@@ -0,0 +1,13 @@
+package Atividades;
+
+public class atvd15 {
+	public static void main(String[] args) {
+     
+		for(int i = 0; i < 20; i++) {
+			System.out.println("Eu gosto de estudar Algoritimos!");
+		}
+		
+		
+		
+	}	
+	}
